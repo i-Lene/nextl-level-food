f9bfd10cb5c76ecff10b46bcd510eb0bdc8149dd
i-Lene <marlene.s.veiga@gmail.com>
2024-12-16 22:26:06 +0000

    commit

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -3,6 +3,7 @@ import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 import Image from "next/image";
 import MainHeaderBackground from "./main-hader-backgrtround";
+import NavLink from "./nav-link";
 
 export function MainHeader() {
   return (
@@ -17,10 +18,10 @@ export function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Meals</Link>
+              <NavLink href="/meals">Meals</NavLink>
             </li>
             <li>
-              <Link href="/community">Community</Link>
+              <NavLink href="/community">Community</NavLink>
             </li>
           </ul>
         </nav>
