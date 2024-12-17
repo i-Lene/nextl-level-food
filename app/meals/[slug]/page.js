c31f24420fb6c76d6074b9d57f156f0ccdd6f490
i-Lene <marlene.s.veiga@gmail.com>
2024-12-17 22:44:23 +0000

    share page

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,10 +1,40 @@
 import Link from "next/link";
+import classes from "./page.module.css";
+import Image from "next/image";
+import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
 
-export default function MealsSlug({params}) {
+export default function MealsSlug({ params }) {
+  const slug = params.slug;
+  const meal = getMeal(slug);
+
+  if (!meal) {
+    notFound();
+  }
+  
+  meal.instructions = meal.instructions.replace(/\n/g, "<br>");
   return (
-    <main>
-      <h1>Meals Slug</h1>
-      <p>{params.slug}</p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} fill alt={meal.title} />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+          className={classes.instructions}
+        ></p>
+      </main>
+    </>
   );
 }
